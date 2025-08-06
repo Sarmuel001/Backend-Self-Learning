7852420850da503b05598ff09b5855dce10bc965
sarmuel001 <adebayosunday51@gmail.com>
2025-08-06 23:16:42 +0100

    Day 7 - Still on Day7

--- Express/app.js
+++ Express/app.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 const port = 3000;
 app.get('/',(req,res)=>{
     res.status(200)
     .json({Name:'Sam Adebayo', SChool:'Jason UNiversity'})
+    
+})
+
+app.get('/',(req,res)=>{
+    res.send('Hello from the server side')
 })
 
 //making a POST express.request
